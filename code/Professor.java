9fa5aec8b9fd594cae5ef72b75fecb9f34a4d93a
gabrielfaria13 <130709840+gabrielfaria13@users.noreply.github.com>
2024-08-25 21:46:07 -0300

    Update Professor.java

--- code/Professor.java
+++ code/Professor.java
@@ -1,8 +1,6 @@
 import java.util.List;
 
 public class Professor extends Usuario{
-    private String nome;
-    private int id;
     private List<Turma> turmas;
 
     public void visualizarAlunos(){
